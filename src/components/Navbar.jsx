872b1ad7f0078d78e3dc940824c92e9e5c7bce33
tomiwashelby <tomiwatolugbemi@gmail.com>
2024-12-23 00:38:34 +0100

    new change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
 	return (
 		<nav className='h-[72px] sm:h-20 md:h-24 flex justify-center items-center'>
 			<div className="cursor-default flex justify-center items-center gap-2">
-				<CgGym className='text-4xl sm:text-5xl text-blue-400'/>
-				<div className='text-2xl sm:text-4xl uppercase font-bold'>
+				<CgGym className='text-2xl sm:text-2xl md:text-3xl text-blue-400'/>
+				<div className='text-lg sm:text-xl md:text-2xl uppercase font-bold'>
 					<span>swole</span><span className='text-blue-400'>normous</span>
 				</div>
 			</div>
